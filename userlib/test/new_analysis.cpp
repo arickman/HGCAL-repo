0f7e246a147d5fd17ae5093956b1e4ebe440d553
arickman <arickman@stanford.edu>
2016-06-23 16:49:07 -0700

    next

--- userlib/test/new_analysis.cpp
+++ userlib/test/new_analysis.cpp
@@ -46,8 +46,8 @@ int main(int argc, char** argv) {
 	TFile *infile = TFile::Open(argv[1]);
 	TTree *tree = (TTree*) infile->Get("HGCSSTree");
 
-	std::vector<HGCSSEvent> * EventVec = 0;
-	tree->SetBranchAddress("HGCSSEventVec", &EventVec);
+	//std::vector<HGCSSEvent> * EventVec = 0;
+	//tree->SetBranchAddress("HGCSSEventVec", &EventVec);
 
 	std::vector<HGCSSGenParticle> * HadVec = 0;
 	tree->SetBranchAddress("HGCSSHadAction", &HadVec);
